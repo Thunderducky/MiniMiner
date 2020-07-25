ac3982ed88fe4a35ce98be34ad8dedd805937f86
Thunderducky <scottzillaster@gmail.com>
2020-07-25 23:41:11 +0000

    working on some basics

--- dist/index.js
+++ dist/index.js
@@ -1,2 +1,117 @@
 "use strict";
-console.log("test");
+Object.defineProperty(exports, "__esModule", { value: true });
+class Asteroid {
+    constructor(designation, x, y, value) {
+        this.designation = designation;
+        this.x = x;
+        this.y = y;
+        this.dx = (Math.random() - 0.5) * 2;
+        this.dy = (Math.random() - 0.5) * 2;
+        this.value = value;
+    }
+}
+class GameWorldData {
+    constructor() {
+        this.asteroids = [];
+        this.asteroids.push(new Asteroid("A", 2, 4, 10));
+        this.asteroids.push(new Asteroid("B", 20, 8, 20));
+        this.asteroids.push(new Asteroid("C", 1, 1, 4));
+    }
+}
+// We might keep these as debug flags and this will allow us to dynamically insert strings, but then treat it as enums
+const SIMULATE = "physics.simulate.step";
+// physicsReducer
+// step the simulation
+// update new value, including moving all the asteroids
+class GameWorldStore {
+    constructor() {
+        this._worldData = new GameWorldData();
+    }
+    static get instance() {
+        if (!this._instance) {
+            this._instance = new GameWorldStore();
+        }
+        return this._instance;
+    }
+    get worldData() {
+        return this._worldData;
+    }
+    dispatch(message) {
+        // handle things in a reducer here, and allow us to build pipes of reducers, these can affect certain parts of the gameworld, so we can more precisely control
+        // mutations
+        // they can also access any other part of the world that they need in a readonly fashion if necessary
+        console.log("This is where we submit actions that happen to the game world", message);
+        if (message === SIMULATE) {
+            // we run the physics simulation, which will be the following
+            this._worldData.asteroids.forEach((asteroid) => {
+                asteroid.x += asteroid.dx;
+                asteroid.y += asteroid.dy;
+            });
+        }
+    }
+}
+const { asteroids } = GameWorldStore.instance.worldData;
+const displayAsteroids = (asteroids) => {
+    asteroids.forEach(asteroid => console.log(`
+Designation: ${asteroid.designation} 
+Position: (${asteroid.x}, ${asteroid.y})
+Velocity: (${asteroid.dx}, ${asteroid.dy})
+Value: $${asteroid.value}m
+`));
+};
+displayAsteroids(asteroids);
+GameWorldStore.instance.dispatch(SIMULATE);
+GameWorldStore.instance.dispatch(SIMULATE);
+GameWorldStore.instance.dispatch(SIMULATE);
+displayAsteroids(asteroids);
+// Let's try and draw this
+// Let's draw a grid so we can see what is happening over time, we'll divide it up into grid areas and then we'll be able to see
+class Camera {
+    constructor(top, left, width, height) {
+        this._width = width;
+        this._height = height;
+        this._halfWidth = width / 2;
+        this._halfHeight = height / 2;
+        this._top = top;
+        this._left = left;
+        this._right = left + width;
+        this._bottom = top + height;
+    }
+    get center() {
+        return { x: this._left + this._halfWidth, y: this._top + this._halfHeight };
+    }
+    set center(p) {
+        this._top = p.y - this._halfHeight;
+        this._left = p.x - this._halfWidth;
+    }
+    get width() {
+        return this._width;
+    }
+    get height() {
+        return this._height;
+    }
+    get left() {
+        return this._left;
+    }
+    get right() {
+        return this._right;
+    }
+    get top() {
+        return this._top;
+    }
+    get bottom() {
+        return this._bottom;
+    }
+    contains(p) {
+        const { _top: top, _left: left, _right: right, _bottom: bottom } = this;
+        const { x, y } = p;
+        return left <= x && x <= right
+            && bottom <= y && y <= top;
+    }
+}
+const camera = new Camera(-10, -10, 20, 20);
+const visibleAsteroids = asteroids.filter(asteroid => camera.contains(asteroid));
+const relativePositions = asteroids.map(asteroid => {
+    return Object.assign(Object.assign({}, asteroid), { x: asteroid.x - camera.left, y: asteroid.y - camera.top });
+});
+// let's draw them on the screen
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,162 @@
-console.log("test");
+import { DeepReadonly } from 'ts-essentials';
+import chalk from 'chalk';
+
+class Asteroid {
+    designation: string;
+    x: number;
+    y: number;
+    value: number;
+    dx: number;
+    dy: number;
+    
+    constructor(designation:string, x:number, y:number, value:number){
+        this.designation = designation;
+        this.x = x;
+        this.y = y;
+        this.dx = (Math.random() - 0.5)*2;
+        this.dy = (Math.random() - 0.5)*2;
+        this.value = value;
+    }
+}
+
+class GameWorldData {
+    asteroids: Asteroid[];
+    constructor(){
+        this.asteroids = [];
+        this.asteroids.push(new Asteroid("A", 2,4,10));
+        this.asteroids.push(new Asteroid("B", 20,8,20));
+        this.asteroids.push(new Asteroid("C", 1,1,4));
+    }
+}
+
+// We might keep these as debug flags and this will allow us to dynamically insert strings, but then treat it as enums
+const SIMULATE = "physics.simulate.step"
+
+// physicsReducer
+// step the simulation
+// update new value, including moving all the asteroids
+
+class GameWorldStore {
+    // Singleton Pieces
+    private static _instance: GameWorldStore;
+    public static get instance(): DeepReadonly<GameWorldStore> {
+        if (!this._instance) {
+            this._instance = new GameWorldStore();
+        }
+        return this._instance;
+    }
+    // Instance Pieces
+    private _worldData: GameWorldData;
+    private constructor() {
+        this._worldData = new GameWorldData();
+    }
+    get worldData(): DeepReadonly<GameWorldData> {
+        return this._worldData;
+    }
+    
+    dispatch(message:string){
+        // handle things in a reducer here, and allow us to build pipes of reducers, these can affect certain parts of the gameworld, so we can more precisely control
+        // mutations
+        // they can also access any other part of the world that they need in a readonly fashion if necessary
+        console.log("This is where we submit actions that happen to the game world", message);
+        if(message === SIMULATE){
+            // we run the physics simulation, which will be the following
+            this._worldData.asteroids.forEach(
+                (asteroid) => {
+                    asteroid.x += asteroid.dx; 
+                    asteroid.y += asteroid.dy;
+                });
+        }
+    }
+
+}
+const { asteroids } = GameWorldStore.instance.worldData;
+type AsteroidArray = typeof asteroids;
+
+const displayAsteroids = (asteroids: AsteroidArray) =>{
+asteroids.forEach(
+    asteroid => console.log(
+`
+Designation: ${asteroid.designation} 
+Position: (${asteroid.x}, ${asteroid.y})
+Velocity: (${asteroid.dx}, ${asteroid.dy})
+Value: $${asteroid.value}m
+`));
+}
+
+displayAsteroids(asteroids);
+GameWorldStore.instance.dispatch(SIMULATE);
+GameWorldStore.instance.dispatch(SIMULATE);
+GameWorldStore.instance.dispatch(SIMULATE);
+displayAsteroids(asteroids);
+
+// Let's try and draw this
+// Let's draw a grid so we can see what is happening over time, we'll divide it up into grid areas and then we'll be able to see
+class Camera {
+    private _top: number;
+    private _left: number;
+    private _bottom: number;
+    private _right: number;
+    private _width: number;
+    private _height: number;
+    private _halfHeight: number;
+    private _halfWidth: number;
+    
+    constructor(top:number, left:number, width:number, height:number){
+        this._width = width;
+        this._height = height;
+        this._halfWidth = width/2;
+        this._halfHeight = height/2;
+        this._top = top;
+        this._left = left;
+        this._right = left + width;
+        this._bottom = top + height;
+    }
+    get center(){
+        return { x: this._left + this._halfWidth, y: this._top + this._halfHeight};
+    }
+    set center(p: {x:number, y:number}){
+        this._top = p.y - this._halfHeight;
+        this._left = p.x - this._halfWidth;
+    }
+    get width(){
+        return this._width;
+    }
+    get height(){
+        return this._height;
+    }
+    get left() {
+        return this._left;
+    }
+    get right() {
+        return this._right;
+    }
+     get top() {
+        return this._top;
+    }
+    get bottom() {
+        return this._bottom;
+    }
+    contains(p: {x:number, y:number}):boolean{
+        const { _top:top, _left:left, _right:right, _bottom:bottom } = this;
+        const {x,y} = p;
+
+        return left <= x && x <= right 
+            && bottom <= y && y <= top;
+    }
+}
+
+const camera = new Camera(-10, -10, 20, 20);
+const visibleAsteroids = asteroids.filter(asteroid => camera.contains(asteroid));
+
+const relativePositions = asteroids.map(asteroid => {
+    return {
+        ...asteroid,
+        x: asteroid.x - camera.left,
+        y: asteroid.y - camera.top
+    }
+})
+// let's draw them on the screen
+
+
+
