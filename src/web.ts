5b3552ac0c9b3ab84525c0ccc839031c79b385ae
Thunderducky <scottzillaster@gmail.com>
2020-07-29 06:24:50 +0000

    added some text examples

--- src/web.ts
+++ src/web.ts
@@ -3,11 +3,28 @@ import * as PIXI from 'pixi.js'
 const app = new PIXI.Application({ backgroundColor: 0x002233 });
 document.body.appendChild(app.view);
 
-// const basicText = new PIXI.Text('Basic text in pixi');
-// basicText.x = 50;
-// basicText.y = 100;
+const basicText = new PIXI.Text('asteroid:\n ', { fontFamily: "monospace", fill: "#ddeeff"});
+basicText.x = 10;
+basicText.y = 10;
 
-// app.stage.addChild(basicText);
+function addTextOverTime(textObject:PIXI.Text, finalString:string, timing:number){
+    return new Promise((resolve) => {
+        const timePerCharacter = timing/finalString.length;
+        let i = 0;
+        const textTimer = setInterval(() => {
+            textObject.text += finalString[i];
+            i++;
+            if(i >= finalString.length){
+                clearInterval(textTimer);
+                resolve()
+            }
+        }, timePerCharacter)
+    })
+}
+
+addTextOverTime(basicText, "designation: AX-23-45", 3000).then(() => console.log("done"))
+
+app.stage.addChild(basicText);
 
 // const style = new PIXI.TextStyle({
 //     fontFamily: 'Arial',
@@ -33,30 +50,29 @@ document.body.appendChild(app.view);
 // app.stage.addChild(richText);
 // // Draw an asteroid in aseprite
 PIXI.Texture.fromURL('/assets/Asteroid1.png').then(texture => {
-const asteroid = new PIXI.Sprite(texture);
-asteroid.anchor.x = 0.5;
-asteroid.anchor.y = 0.5;
-asteroid.position.x = 100;
-asteroid.position.y = 150;
+    const asteroid = new PIXI.Sprite(texture);
+    asteroid.anchor.x = 0.5;
+    asteroid.anchor.y = 0.5;
+    asteroid.position.x = 100;
+    asteroid.position.y = 150;
 
-const asteroid2 = new PIXI.Sprite(texture);
-asteroid2.anchor.x = 0.5;
-asteroid2.anchor.y = 0.5;
-asteroid2.position.x = 200;
-asteroid2.position.y = 170;
+    const asteroid2 = new PIXI.Sprite(texture);
+    asteroid2.anchor.x = 0.5;
+    asteroid2.anchor.y = 0.5;
+    asteroid2.position.x = 200;
+    asteroid2.position.y = 170;
 
-app.stage.addChild(asteroid);
-app.stage.addChild(asteroid2);
+    app.stage.addChild(asteroid);
+    app.stage.addChild(asteroid2);
 
-animate();
-function animate() {
-    requestAnimationFrame(animate);
+    animate();
+    function animate() {
+        requestAnimationFrame(animate);
 
-    // just for fun, let's rotate mr rabbit a little
-    asteroid.rotation += 0.01;
-    asteroid2.rotation += 0.02;
+        asteroid.rotation += 0.01;
+        asteroid2.rotation += 0.02;
 
-    // render the container
-    app.renderer.render(app.stage);
-}
+        // render the container
+        app.renderer.render(app.stage);
+    }
 });
