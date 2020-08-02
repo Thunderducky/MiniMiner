e1932880f0836de92d94cc8a4ed4da0734c1cd37
Thunderducky <scottzillaster@gmail.com>
2020-08-02 00:06:09 +0000

    added some basic drawing code

--- src/web.ts
+++ src/web.ts
@@ -1,78 +1,57 @@
 // Let's start by importing PIXI and making an application and just drawing some text
 import * as PIXI from 'pixi.js'
-const app = new PIXI.Application({ backgroundColor: 0x002233 });
-document.body.appendChild(app.view);
+window.onload = function(){
+    const app = new PIXI.Application({ backgroundColor: 0x002233 });
+    app.renderer.resize(window.innerWidth, window.innerHeight);
+    document.body.appendChild(app.view);
 
-const basicText = new PIXI.Text('asteroid:\n ', { fontFamily: "monospace", fill: "#ddeeff"});
-basicText.x = 10;
-basicText.y = 10;
+    const basicText = new PIXI.Text('asteroid:\n ', { fontFamily: "monospace", fill: "#ddeeff"});
+    basicText.x = 10;
+    basicText.y = 10;
 
-function addTextOverTime(textObject:PIXI.Text, finalString:string, timing:number){
-    return new Promise((resolve) => {
-        const timePerCharacter = timing/finalString.length;
-        let i = 0;
-        const textTimer = setInterval(() => {
-            textObject.text += finalString[i];
-            i++;
-            if(i >= finalString.length){
-                clearInterval(textTimer);
-                resolve()
-            }
-        }, timePerCharacter)
+    // Let's use the loader and get all the assets we need
+    app.loader.baseUrl = "assets";
+    app.loader
+        .add("asteroid", "images/Asteroid1.png")
+        .add("background", "images/bg5.jpg", {})
+        .add("playerShip", "images/orangeship.png")
+        .add("station", "images/SS1.png");
+
+    app.loader.load(function(){
+        const { resources } = app.loader;
+
+        const background = new PIXI.Sprite(resources["background"].texture);
+        console.log(background);
+        background.anchor.x = 0.5;
+        background.anchor.y = 0.5;
+        background.position.x = 0;
+        background.position.y = 0;
+        app.stage.addChild(background);
+
+        const asteroid = new PIXI.Sprite(resources["asteroid"].texture);
+        asteroid.anchor.x = 0.5;
+        asteroid.anchor.y = 0.5;
+        asteroid.position.x = 100;
+        asteroid.position.y = 100;
+        app.stage.addChild(asteroid);
+
+        const ship = new PIXI.Sprite(resources["playerShip"].texture);
+        // the ship has now been scaled appropriately
+        ship.scale.set(0.2, 0.2);
+        ship.anchor.x = 0.5;
+        ship.anchor.y = 0.5;
+        ship.position.x = 200;
+        ship.position.y = 200;
+        app.stage.addChild(ship);
+
+        
+        // console.log(loader);
+        // console.log(resources);
+        // const asteroid = new PIXI.Sprite(resources.get("asteroid")
     })
+
 }
-
-addTextOverTime(basicText, "designation: AX-23-45", 3000).then(() => console.log("done"))
-
-app.stage.addChild(basicText);
-
-// const style = new PIXI.TextStyle({
-//     fontFamily: 'Arial',
-//     fontSize: 36,
-//     fontStyle: 'italic',
-//     fontWeight: 'bold',
-//     fill: ['#ffffff', '#00ff99'], // gradient
-//     stroke: '#4a1850',
-//     strokeThickness: 5,
-//     dropShadow: true,
-//     dropShadowColor: '#000000',
-//     dropShadowBlur: 4,
-//     dropShadowAngle: Math.PI / 6,
-//     dropShadowDistance: 6,
-//     wordWrap: true,
-//     wordWrapWidth: 440,
-// });
-
-// const richText = new PIXI.Text('Rich text with a lot of options and across multiple lines', style);
-// richText.x = 50;
-// richText.y = 250;
-
-// app.stage.addChild(richText);
-// // Draw an asteroid in aseprite
-PIXI.Texture.fromURL('/assets/Asteroid1.png').then(texture => {
-    const asteroid = new PIXI.Sprite(texture);
-    asteroid.anchor.x = 0.5;
-    asteroid.anchor.y = 0.5;
-    asteroid.position.x = 100;
-    asteroid.position.y = 150;
-
-    const asteroid2 = new PIXI.Sprite(texture);
-    asteroid2.anchor.x = 0.5;
-    asteroid2.anchor.y = 0.5;
-    asteroid2.position.x = 200;
-    asteroid2.position.y = 170;
-
-    app.stage.addChild(asteroid);
-    app.stage.addChild(asteroid2);
-
-    animate();
-    function animate() {
-        requestAnimationFrame(animate);
-
-        asteroid.rotation += 0.01;
-        asteroid2.rotation += 0.02;
-
-        // render the container
-        app.renderer.render(app.stage);
-    }
-});
+// Todo, handle scaling
+// Todo, make sure everything renders properly
+// Listen for events
+// More gross UI stuff
