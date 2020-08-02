a5c19de2cad13a3c13021bcc7245ff48110dc169
Thunderducky <scottzillaster@gmail.com>
2020-08-02 00:15:17 +0000

    updated with some simple logic

--- src/web.ts
+++ src/web.ts
@@ -24,16 +24,23 @@ window.onload = function(){
         console.log(background);
         background.anchor.x = 0.5;
         background.anchor.y = 0.5;
-        background.position.x = 0;
-        background.position.y = 0;
+        background.position.x = app.view.width/2;
+        background.position.y = app.view.height/2;
         app.stage.addChild(background);
-
-        const asteroid = new PIXI.Sprite(resources["asteroid"].texture);
-        asteroid.anchor.x = 0.5;
-        asteroid.anchor.y = 0.5;
-        asteroid.position.x = 100;
-        asteroid.position.y = 100;
-        app.stage.addChild(asteroid);
+        (window as any).background = background;
+        const asteroids = [];
+        function makeAsteroid(x:number,y:number){
+            const asteroid = new PIXI.Sprite(resources["asteroid"].texture);
+            asteroid.anchor.x = 0.5;
+            asteroid.anchor.y = 0.5;
+            asteroid.position.x = x;
+            asteroid.position.y = y;
+            asteroids.push(asteroid);
+            app.stage.addChild(asteroid);
+        }
+        makeAsteroid(100, 100);
+        makeAsteroid(200, 300);
+        makeAsteroid(300, 150);
 
         const ship = new PIXI.Sprite(resources["playerShip"].texture);
         // the ship has now been scaled appropriately
@@ -43,8 +50,29 @@ window.onload = function(){
         ship.position.x = 200;
         ship.position.y = 200;
         app.stage.addChild(ship);
+        (window as any).ship = ship;
 
-        
+        const station = new PIXI.Sprite(resources["station"].texture);
+        // the ship has now been scaled appropriately
+        station.scale.set(0.4, 0.4);
+        station.anchor.x = 0.5;
+        station.anchor.y = 0.5;
+        station.position.x = 1000;
+        station.position.y = 800;
+        app.stage.addChild(station);
+        (window as any).station = station;
+
+        // let's have the station slowly rotate
+
+        animate();
+        function animate() {
+            requestAnimationFrame(animate);
+
+            station.rotation += 0.0005;
+
+            // render the container
+            app.renderer.render(app.stage);
+        }
         // console.log(loader);
         // console.log(resources);
         // const asteroid = new PIXI.Sprite(resources.get("asteroid")
